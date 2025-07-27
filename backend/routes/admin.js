11e97ad27ede84f680386db1311623fc17f1f275
sadafshahab12 <sadafshahab123@yahoo.com>
2025-07-27 15:53:03 +0500

    done

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -1,16 +1,18 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Admin = require('../models/Admin');
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcryptjs');
+const Admin = require("../models/Admin");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 
-router.post('/login', async (req, res) => {
+router.post("/login", async (req, res) => {
   const { username, password } = req.body;
   const admin = await Admin.findOne({ username });
-  if (!admin || !(await bcrypt.compare(password, admin.password))) {
-    return res.status(401).json({ message: 'Invalid credentials' });
+  if (!admin || !bcrypt.compare(password, admin.password)) {
+    return res.status(401).json({ message: "Invalid credentials" });
   }
-  const token = jwt.sign({ id: admin._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+  const token = jwt.sign({ id: admin._id }, process.env.JWT_SECRET, {
+    expiresIn: "1h",
+  });
   res.json({ token });
 });
 
--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,26 @@
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const feedbackRoutes = require('./routes/feedback.js');
-const adminRoutes = require('./routes/admin.js');
+const dotnev = require("dotenv")
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const feedbackRoutes = require("./routes/feedback.js");
+const adminRoutes = require("./routes/admin.js");
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin : process.env.FRONTEND_URL
+}));
 app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("Feedback management system");
+});
+app.use("/feedback", feedbackRoutes);
+app.use("/admin", adminRoutes);
 
-app.use('/feedback', feedbackRoutes);
-app.use('/admin', adminRoutes);
-
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => app.listen(5000, () => console.log('Server running on port 5000')))
-  .catch(err => console.error(err));
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() =>
+    app.listen(5000, () =>
+      console.log(`Server running on port http://localhost:${5000}`)
+    )
+  )
+  .catch((err) => console.error(err));
